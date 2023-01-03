f38139e70af52fe7c1fd34bf98c445c9ecf7db5b
Adarsh Dubey <dubeyadarshmain@gmail.com>
2023-01-04 01:22:52 +0530

    Basic reset, yet again!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import './styles/App.scss'
-import Header from './components/Header'
-import Main from './components/Main'
-
 function App() {
   return (
     <div className="container">
-      <Header />
-      <Main />
+      Hello World!
     </div>
   );
 }
