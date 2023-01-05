5d94b68195b7cd3ee9ef112f7e1cacfe121617eb
Adarsh Dubey <dubeyadarshmain@gmail.com>
2023-01-05 23:02:57 +0530

    Added data files with little fake data

---
+++ src/data/NavData.js
@@ -0,0 +1,7 @@
+const NavData = {
+    ourWebsite: 'https://ghw-schedule.netlify.app',
+    officialWebsite: 'https://hackp.ac/January',
+    github: 'https://github.com/bachelors-in-open-source/ghw-schedule'
+}
+
+export default NavData
---
+++ src/data/ScheduleData.js
@@ -0,0 +1,34 @@
+const ScheduleData = [
+  {
+    dateDay: "7th Dec, Monday",
+    events: [
+      {
+        time: "10:30 pm",
+        title: "Trying out new technologies with MLH Coaches",
+        link: "https://sampleLinkHere.html",
+      },
+    ],
+  },
+  {
+    dateDay: "7th Dec, Monday",
+    events: [
+      {
+        time: "10:30 pm",
+        title: "Trying out new technologies with MLH Coaches",
+        link: "https://sampleLinkHere.html",
+      },
+    ],
+  },
+  {
+    dateDay: "7th Dec, Monday",
+    events: [
+      {
+        time: "10:30 pm",
+        title: "Trying out new technologies with MLH Coaches",
+        link: "https://sampleLinkHere.html",
+      },
+    ],
+  },
+];
+
+export default ScheduleData
